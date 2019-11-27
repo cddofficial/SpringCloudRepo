d4d0f9b82dcaa24e34c34a24082cf0cc6fb37c61
cdd <cdd@192.168.50.12>
2019-11-27 13:42:12 +0800

    Dashboard展示监控微服务监控信息实例

---
+++ hystrix-dashboard-demo/src/main/java/com/cdd/demo/HystrixDashboardDemoApplication.java
@@ -0,0 +1,14 @@
+package com.cdd.demo;
+ 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+ 
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardDemoApplication {
+ 
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixDashboardDemoApplication.class, args);
+	}
+}
