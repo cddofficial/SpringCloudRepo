03dfb6cc2750214347ee4927e64005175509daf7
cddofficial <cdd.personal@foxmail.com>
2019-12-18 16:36:06 +0800

    配置属性刷新

---
+++ microservice-config-client-refresh/src/test/java/com/cdd/demo/EurekaServerServiceDiscoverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdd.demo;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerServiceDiscoverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
